3ae1238635403cf9cdf43fd5294f13496761c4a9
qmk <wherlicq@gmail.com>
2020-05-20 23:05:25 +0200

    Hotfix unix installation scripts for installation folder.

--- installers/mac_installer.sh
+++ installers/mac_installer.sh
@@ -3,8 +3,8 @@ brew cask install docker; open /Applications/Docker.app
 # Install Git
 git --version
 # Download project
-mkdir /usr/local/bin/qmk
-git -C /usr/local/bin/qmk clone https://github.com/hQamonky/YtMusicDownloader.git
+mkdir ~/qmk
+git -C ~/qmk clone https://github.com/hQamonky/YtMusicDownloader.git
 # Setup docker container
-sudo docker build --no-cache -t qmk_yt_music_dl /usr/local/bin/qmk/YtMusicDownloader
-sudo docker run -p 8092:8080 -v /usr/local/bin/qmk/YtMusicDownloader:/usr/src/app -v ~/Music:/Music -d --name qmk_ymd qmk_yt_music_dl
+sudo docker build --no-cache -t qmk_yt_music_dl ~/qmk/YtMusicDownloader
+sudo docker run -p 8092:8080 -v ~/qmk/YtMusicDownloader:/usr/src/app -v ~/Music:/Music -d --name qmk_ymd qmk_yt_music_dl
